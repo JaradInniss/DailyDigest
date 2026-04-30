e9630e4efc3eeb1196659da5b8571e76b07cbc04
Jarad Inniss <jjinniss02@gmail.com>
2026-04-30 14:40:58 +0800

    Next.js image load fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: "C:/Users/jjinn/OpenCode Projects/NewsApp",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
